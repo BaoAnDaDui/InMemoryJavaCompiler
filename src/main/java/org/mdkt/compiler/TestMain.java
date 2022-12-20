a85df476b69c8ff5c339ccd926729cdbffafc3c1
wangxiao <wxiao1002@yeah.net>
2022-12-20 09:33:59 +0800

    add test main

---
+++ src/main/java/org/mdkt/compiler/TestMain.java
@@ -0,0 +1,24 @@
+package org.mdkt.compiler;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author wang xiao
+ * date 2022/12/20
+ */
+public class TestMain {
+
+
+    public static void main(String[] args) throws Exception {
+        StringBuilder sourceCode = new StringBuilder();
+        sourceCode.append("package org.mdkt;\n");
+        sourceCode.append("public class HelloClass {\n");
+        sourceCode.append("   public String hello() { return \"hello\"; }");
+        sourceCode.append("}");
+
+        Class<?> helloClass = InMemoryJavaCompiler.newInstance().compile("org.mdkt.HelloClass", sourceCode.toString());
+        Method helloClassMethod = helloClass.getMethod("hello");
+        Object invoke = helloClassMethod.invoke(helloClass.getDeclaredConstructor().newInstance());
+        System.out.println(invoke);
+    }
+}
